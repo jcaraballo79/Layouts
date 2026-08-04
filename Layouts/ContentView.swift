09e7038f0923e9b359fa19ac12ec9cf3305ac847
Jorge J. Caraballo <jcaraballo79@gmail.com>
2026-08-04 18:49:36 -0400

    Ch. 1. SwiftUI Fundamentals: Getting Layout Right Pt. 1 Frames, multilineAlignment, maxWidth from Prof. Gallaugher's SwiftUI Course

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = "When the genius bar needs help, they call you!"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .border(.blue)
+                .padding()
+            
+            HStack {
+                Button {
+                    messageString = "You Are Awesome!"
+                } label: {
+                    Text("Awesome")
+                }
+                
+                Button {
+                    messageString = "You Are Great!"
+                } label: {
+                    Text("Great")
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle)
+            .border(.purple)
         }
-        .padding()
     }
 }
 
