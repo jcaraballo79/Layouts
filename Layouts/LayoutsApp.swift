12e0b27f269f114d6b4724b740b0f920fff64517
Jorge J. Caraballo <jcaraballo79@gmail.com>
2026-08-04 17:46:10 -0400

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Jorge Caraballo on 8/4/26.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
